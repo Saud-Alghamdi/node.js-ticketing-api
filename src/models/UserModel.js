618522802f65f4b5d64a7a64a9cdc79a40c240d4
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2023-08-28 19:39:11 +0300

    Modify User Model after abstracting db config in a seprate file

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,23 +1,4 @@
-const dotenv = require("dotenv");
-dotenv.config();
-
-const DB_HOST = process.env.DB_HOST;
-const DB_NAME = process.env.DB_NAME;
-const DB_USER = process.env.DB_USER;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_TYPE = process.env.DB_TYPE;
-
-const { Sequelize, DataTypes, Model } = require("sequelize");
-
-const db = new Sequelize(`${DB_NAME}`, `${DB_USER}`, `${DB_PASSWORD}`, {
-  host: `${DB_HOST}`,
-  dialect: `${DB_TYPE}`,
-});
-
-// Check DB Connection
-db.authenticate()
-  .then(() => console.log("Connection has been established successfully."))
-  .catch((error) => console.error("Unable to connect to the database:", error));
+const { db, Model, DataTypes } = require("./dbConfig");
 
 // Create User table in DB if it doesn't exist
 db.sync()
