fb25e3646f485e48c3b7c555983a0f05a53f8903
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2023-08-28 19:38:29 +0300

    Create Ticket Model

---
+++ src/models/TicketModel.js
@@ -0,0 +1,39 @@
+const { db, Model, DataTypes } = require("./dbConfig");
+
+// Create Ticket table in DB if it doesn't exist
+db.sync()
+  .then(() => console.log("Ticket Model has been synchronized successfully."))
+  .catch((error) => console.error("Error syncing Ticket model:", error));
+
+class Ticket extends Model {}
+
+Ticket.init(
+  {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: "Open",
+    },
+    assignedTo: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: Ticket,
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize: db,
+    modelName: "Ticket",
+  }
+);
+
+module.exports = Ticket;
