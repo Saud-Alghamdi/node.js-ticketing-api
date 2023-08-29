c2431d432e69148380433e2d747ff8107833dc5b
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2023-08-30 00:16:21 +0300

    Add ticket routes middleware

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
 const userRoutes = require("./routes/userRoutes");
+const ticketRoutes = require("./routes/ticketRoutes");
 
 // Parse json body
 app.use(express.json());
 
 app.use("/api", userRoutes);
+app.use("/api", ticketRoutes);
 
 app.listen(port, () => console.log(`Listening to request on port ${port}`));
