d3c9aa97b8ed91d4fdbbe77c9ae7432f69be5de8
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2023-08-29 20:59:59 +0300

    Add getUser and getUsers routes, and also modify route paths

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,11 +3,17 @@ const router = express.Router();
 const createUser = require("../controllers/user/createUser");
 const deleteUser = require("../controllers/user/deleteUser");
 const updateUser = require("../controllers/user/updateUser");
+const getUser = require("../controllers/user/getUser");
+const getUsers = require("../controllers/user/getUsers");
 
-router.post("/users/createUser", async (req, res) => await createUser(req, res));
+router.post("/users", async (req, res) => await createUser(req, res));
 
-router.delete("/users/deleteUser/:id", async (req, res) => await deleteUser(req, res));
+router.delete("/users/:id", async (req, res) => await deleteUser(req, res));
 
-router.put("/users/updateUser/:id", async (req, res) => await updateUser(req, res));
+router.put("/users/:id", async (req, res) => await updateUser(req, res));
+
+router.get("/users/:id", async (req, res) => await getUser(req, res));
+
+router.get("/users", async (req, res) => await getUsers(req, res));
 
 module.exports = router;
