3e9353c26a55d0cffa7ada11183a6ecdcc00ff9b
Saud-Alghamdi <saudalghamdi1997@gmail.com>
2023-08-30 00:58:20 +0300

    Create a middleware that checks all necessary request fields are present

---
+++ src/middleware/checkAllFieldsPresent.js
@@ -0,0 +1,12 @@
+function checkAllFieldsPresent(requiredFields) {
+  return function (req, res, next) {
+    for (const field of requiredFields) {
+      if (!req.body[field]) {
+        return res.status(400).json({ isSuccess: false, message: `${field} field is required` });
+      }
+    }
+    next();
+  };
+}
+
+module.exports = checkAllFieldsPresent;
